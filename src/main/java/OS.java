3f9dc29e75f6abfb8910c21939e7e66e7376cc4b
kshammer <hammerschmiks@vcu.edu>
2017-12-01 13:19:29 -0500

    made some scheduler changes

--- src/main/java/OS.java
+++ src/main/java/OS.java
@@ -37,8 +37,13 @@ public class OS {
 
         }
         if(update.equals("done")){
-            System.out.println("A PROCESS HAS FINISHEDJ");
-            cpu.Swap(scheduler.getNextProcess());
+            System.out.println("A PROCESS HAS FINISHED");
+            if(scheduler.programs.readyQueue.isEmpty()){
+
+            }else{
+                cpu.Swap(scheduler.getNextProcess());
+            }
+
             scheduler.resetQuantum();
         }
         if(update.equals("YIELD")){
